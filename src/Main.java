744401976290ab2f1473c5f949bbbfe140ab5201
WaldyWhite <w.weis@hotmail.com>
2023-09-10 18:40:10 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 import java.util.Arrays;
 
 public class Main {
-    public static void main (String[] args) {
-        char [][] symbols = new char[4][6];
+    public static void main(String[] args) {
+        char[][] symbols = new char[4][7];
         char l = 'a';
-
+        char k = 'a';
         for (char[] el : symbols) {
             for (int i = 0; i < el.length; i++) {
-                el[i] = l;
-                l++;
-                System.out.print(el[i]+" ");
+                if (i == 0) {
+                    el[i] = l;
+                } else {
+                    el[i] = k;
+                }
+                k += (char) symbols.length;
+                System.out.print(el[i] + " ");
             }
+            l++;
+            k = l;
             System.out.println();
         }
     }
